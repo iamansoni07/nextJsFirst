21ec64cb34db162fb3aaa19160449419f1b368cd
iamansoni07 <aman.soni0713@gmail.com>
2024-12-20 10:00:00 +0530

    exporting schemas

---
+++ backend_sanity/schemaTypes/index.js
@@ -0,0 +1,15 @@
+// schemaTypes/index.js
+
+import testimonials from './testimonials'  // Import your testimonials schema
+import works from './works'
+import brands from './brands'
+import about from './about'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+
+
+// Export all schemas in an array
+export const schemaTypes = [works, testimonials, brands, about, skills, workExperience, experiences, contact]
